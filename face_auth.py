a8e07ba60aa7c5975a5ef90e644c3ff9cf015d13
RishithaElevate <rishitharajjoudula@gmail.com>
2025-10-07 23:01:11 +0530

    Update face_auth.py

--- face_auth.py
+++ face_auth.py
@@ -1,14 +1,8 @@
-import face_recognition
+from deepface import DeepFace
 
-def verify_face(reference_path, test_path):
+def verify_face(img1_path, img2_path):
     try:
-        ref_image = face_recognition.load_image_file(reference_path)
-        ref_encoding = face_recognition.face_encodings(ref_image)[0]
-
-        test_image = face_recognition.load_image_file(test_path)
-        test_encoding = face_recognition.face_encodings(test_image)[0]
-
-        result = face_recognition.compare_faces([ref_encoding], test_encoding)[0]
-        return result
+        result = DeepFace.verify(img1_path, img2_path)
+        return result["verified"]
     except Exception as e:
-        return f"Error: {str(e)}"
+        return str(e)
