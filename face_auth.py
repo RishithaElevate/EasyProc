90c828e793f9449c48df9194da22d3e5ba4dfcda
RishithaElevate <rishitharajjoudula@gmail.com>
2025-10-16 21:31:52 +0530

    Delete face_auth.py

--- face_auth.py
+++
@@ -1,8 +0,0 @@
-from deepface import DeepFace
-
-def verify_face(img1_path, img2_path):
-    try:
-        result = DeepFace.verify(img1_path, img2_path)
-        return result["verified"]
-    except Exception as e:
-        return str(e)
